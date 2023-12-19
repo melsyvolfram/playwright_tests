4e46d1c5fb52ba91d0b8b681623b2496fcd82056
Mariia Deriabina <mashaerohina@gmail.com>
2023-12-19 14:58:50 +0100

    try different options for ci and config 5

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  workers: process.env.WORKERS ? Number(process.env.WORKERS) : 3,
+  workers: process.env.WORKERS ? Number(process.env.WORKERS) : 2,
   reporter: [
     [
       'html',
@@ -23,14 +23,14 @@ export default defineConfig({
     timeout: 6000,
   },
   use: {
-    baseURL: process.env.BASE_URL || 'https://test-demo.nopcommerce.com',
+    baseURL: process.env.BASE_URL || 'https://demo.nopcommerce.com',
     headless: true,
     viewport: { width: 1280, height: 720 },
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
-    // launchOptions: {
-    //   slowMo: 500,
-    // },
+    launchOptions: {
+      slowMo: process.env.CI ? 0 : 500,
+    },
   },
 
 });
